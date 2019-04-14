d0064859c2e753f7299343b3104a7ecdf878dba6
Kevin Lewis <kvn@lws.io>
2019-04-14 13:24:46 +0100

    og and meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A podcast on running developer events.' }
+      { hid: 'description', name: 'description', content: 'A podcast from developer events company Underland.' },
+      { property: 'og:image', content: 'https://eventhandler.fm/og.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Space+Mono:400,700' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
     ]
   },
 
